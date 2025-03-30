274b025f0e50863f59342710d23964f1bc46c353
book3pro <dhbook420@gmail.com>
2025-03-31 00:18:37 +0900

    test structure

--- pcap-test.c
+++ pcap-test.c
@@ -1,8 +1,9 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "structure.h"
 
-typedef unsigned char u_char;
+
 
 void usage() {
 	printf("syntax: pcap-test <interface>\n");
@@ -37,16 +38,30 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	printf("%s dev\n", param.dev_);
 	while (true) {
 		struct pcap_pkthdr* header;
-		const u_char* packet;
+		const uint8_t* packet;
 		int res = pcap_next_ex(pcap, &header, &packet);
+		//pcap 	 : session handler
+		//header : pointer to structure that holds metadata about packet
+		//			(time sniffed, packet length, lenght of specific portion)
+		//packet : pointer to real data 
+		
 		if (res == 0) continue;
 		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
 		printf("%u bytes captured\n", header->caplen);
+
+		struct ethernet_hdr *eth = (struct ethernet_hdr *)packet;
+		struct ipv4_hdr *ip = (struct ipv4_hdr *)(packet + sizeof(struct ethernet_hdr));
+		struct tcp_hdr *tcp = (struct tcp_hdr *)(packet + sizeof(struct ethernet_hdr) + ip->ip_len);
+
+		printf("ether type : %x\n", eth->ether_type);
+		printf("ip len : %x\n", ip->ip_len);
+		printf("src_port : %x\n", tcp->src_port);
 	}
 
 	pcap_close(pcap);
