ed2b75b7be3fa025edff02cc339a7df9cefd54cd
book3pro <dhbook420@gmail.com>
2025-04-01 16:06:54 +0900

    improve header parsing & print

--- utils.c
+++ utils.c
@@ -7,16 +7,20 @@ void parse_packet (const uint8_t* packet)
 {
     struct ethernet_hdr *eth = (struct ethernet_hdr *)packet;
     struct ipv4_hdr *ip = (struct ipv4_hdr *)(packet + sizeof(struct ethernet_hdr));
-    struct tcp_hdr *tcp = (struct tcp_hdr *)((const uint8_t*)ip + sizeof(struct ipv4_hdr));
-    uint8_t *data_ptr = (uint8_t *)((const uint8_t *)tcp + sizeof(struct tcp_hdr));
 
-    if (eth->ether_type != 8)
+    int ip_hdr_len = ((ip->ip_version_len) & 0x0f) << 2;
+    struct tcp_hdr *tcp = (struct tcp_hdr *)((const uint8_t*)ip + ip_hdr_len);
+
+    //data offset = (first 4bit of tcp_offset_reserved) x 4 
+    int tcp_hdr_len = (((tcp->tcp_offset_reserved) & 0xf0) >> 4) << 2;
+    uint8_t *data_ptr = (uint8_t *)((const uint8_t *)tcp + tcp_hdr_len);
+
+    //check ethertype as IPv4 & tcp protocol 
+    if (ntohs(eth->ether_type) != 0x0800)
         return;
     if (ip->ip_protocol != 6)
-    {
-        
         return;
-    }
+    
     printf("===================Header===================\n");
     printf("Source MAC address       : ");
     print_mac(eth->src_mac_addr);
@@ -33,7 +37,7 @@ void parse_packet (const uint8_t* packet)
     printf("\nHexamedical Data (20 bytes) :\n");
     for (int i = 0; i < 20; i ++)
     {
-        printf("%x ", (uint8_t)*(data_ptr + i));
+        printf("%02x ", (uint8_t)*(data_ptr + i));
     }
     
     printf("\n\n\n");
@@ -41,7 +45,8 @@ void parse_packet (const uint8_t* packet)
 
 void print_mac(uint8_t *mac_addr)
 {
-    printf("%02x:%02x:%02x:%02x:%02x:%02x\n", mac_addr[0],mac_addr[1],mac_addr[2],
+    printf("%02x:%02x:%02x:%02x:%02x:%02x\n", 
+        mac_addr[0],mac_addr[1],mac_addr[2],
         mac_addr[3],mac_addr[4],mac_addr[5]);
 }
 void print_ip(uint32_t ip_addr)
